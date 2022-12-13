a7e6ae5bf5b9a300526f2df5cce7ee440fe1e9ed
deemyboy <deemyboy2@gmail.com>
2022-12-13 09:12:33 +0000

    used spread operator in parameter brackets then used for of loop to add each param to a let total variable this was returned at end of function outside of for loop

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,10 +1,10 @@
 // Instructions can be found in rest_parameters.md
 
-function add(a, b) {
-  // Add a loop here
-  return a + b;
+export function add(...args) {
+    // Add a loop here
+    let total = 0;
+    for (const arg of args) {
+        total += arg;
+    }
+    return total;
 }
-
-add(1, 2, 3, 4, 5);
-
-module.exports = add;
